fe9cce793268d87c81bb4e21b54bf66d5e02fd0a
Ripon Haldar <riponhaldar006@gmail.com>
2026-09-19 02:38:59 +0530

    update mobile

---
+++ mobile/src/screens/categories/CategoriesScreen.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+
+const CategoriesScreen = () => {
+    return (
+        <View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({})
+
+export default CategoriesScreen;
